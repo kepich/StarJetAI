22cf103d692e7f6d20db7ca8c5ec0dd7260f1897
kepich <pav007prikhod@mail.ru>
2019-12-29 01:13:29 +0700

    Обновление объектов и их удаление

--- GameControl.h
+++ GameControl.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <vector>
+#include "GameObject.h"
 #include "Logger.h"
 #include "Spaceship.h"
 #include "SpaceshipFactory.h"
@@ -6,12 +8,27 @@
 class GameControl{
 private:
 	Logger* log = Logger::get_logger();
+	vector<GameObject*> objects = vector<GameObject*>();
+
 public:
 	GameControl() {
 		return;
 	}
 
 	bool update() {
+		// Updating objects
+		for (GameObject* temp_object : this->objects)
+			temp_object->update();
+
+		// Deleting objects
+		GameObject* deleted_object;
+		for (auto temp_obj_iterator = this->objects.begin(); temp_obj_iterator < this->objects.end(); temp_obj_iterator++)
+			if ((*temp_obj_iterator)->is_deleted()) {
+				deleted_object = *temp_obj_iterator;
+				this->objects.erase(temp_obj_iterator);
+				delete deleted_object;
+			}
+
 		log->info("GameControl updating...");
 		return true;
 	}
--- GameObject.h
+++ GameObject.h
@@ -10,10 +10,11 @@ enum GAME_OBJECT_TYPE
 
 class GameObject{
 protected:
-	double	speed[2]	= { 0, 0 };
-	int		position[2] = { 0, 0 };
-	int		type		= EMPTY_OBJECT;
-	int		id			= -1;
+	double	speed[2]		= { 0, 0 };
+	int		position[2]		= { 0, 0 };
+	int		type			= EMPTY_OBJECT;
+	int		id				= -1;
+	bool	is_deleted_flag	= false;
 
 public:
 	GameObject(double speed[2], int position[2], int type, int id) {
@@ -25,6 +26,10 @@ public:
 
 		this->type = type;
 		this->id = id;
+	}
+
+	bool is_deleted() {
+		return this->is_deleted_flag;
 	}
 
 	virtual bool update() = 0;
