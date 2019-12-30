247a15429f597bdfc0a2736298bed07b35904d21
kepich <pav007prikhod@mail.ru>
2019-12-30 11:38:17 +0700

    Collision

--- GameControl.h
+++ GameControl.h
@@ -5,18 +5,44 @@
 #include "Spaceship.h"
 #include "SpaceshipFactory.h"
 
+enum UpdateStatusEnum{
+	ALL_SHIPS_BROKEN,
+	TIME_OUT,
+	UPDATING,
+	PREPARING
+};
+
 class GameControl{
 private:
 	Logger* log = Logger::get_logger();
 	vector<GameObject*> objects = vector<GameObject*>();
+
+	int update_status = PREPARING;
+	int spaceships_count = 0;
+
+	bool test_collision(GameObject* first, GameObject* second) {
+		vector<int>* first_rect = first->get_obj_rect();
+		vector<int>* second_rect = second->get_obj_rect();
+
+		if (((*first_rect)[0] > (*second_rect)[0]) && ((*first_rect)[0] < ((*second_rect)[0] + (*second_rect)[1]))) {
+			if()
+		}
+	}
 
 public:
 	GameControl() {
 		return;
 	}
 
+	~GameControl() {
+		delete log;
+	}
+
 	bool update() {
 		// Updating objects
+		this->update_status = UPDATING;
+		log->info("GameControl updating...");
+
 		for (GameObject* temp_object : this->objects)
 			temp_object->update();
 
@@ -29,13 +55,31 @@ public:
 				delete deleted_object;
 			}
 
-		log->info("GameControl updating...");
+		// Spaceships counting [Teporary] ****************
+		this->spaceships_count = 0;
+		for (GameObject* temp_object : this->objects)
+			if (temp_object->get_type() == SPACESHIP) this->spaceships_count++;
+
+		if (!this->spaceships_count) {
+			this->update_status = ALL_SHIPS_BROKEN;
+			return false;
+		}
+		// ***********************************************
+
 		return true;
 	}
 
 	void render() {
 		log->info("GameControl rendering...");
 		return;
+	}
+
+	int get_update_status() {
+		return this->update_status;
+	}
+
+	void add_object(GameObject* new_object) {
+		this->objects.push_back(new_object);
 	}
 };
 
--- GameObject.h
+++ GameObject.h
@@ -12,17 +12,21 @@ class GameObject{
 protected:
 	double	speed[2]		= { 0, 0 };
 	int		position[2]		= { 0, 0 };
+	int		size[2]			= { 0, 0 };
 	int		type			= EMPTY_OBJECT;
 	int		id				= -1;
 	bool	is_deleted_flag	= false;
 
 public:
-	GameObject(double speed[2], int position[2], int type, int id) {
-		this->speed[0] = speed[0];
+	GameObject(double speed[2], int position[2], int size[2], int type, int id) {
+		this->speed[0] = speed[0];			// Speed vector
 		this->speed[1] = speed[1];
 
-		this->position[0] = position[0];
+		this->position[0] = position[0];	// X; Y coords
 		this->position[1] = position[1];
+
+		this->size[0] = size[0];			// WIDTH; HEIGHT
+		this->size[1] = size[1];
 
 		this->type = type;
 		this->id = id;
@@ -30,6 +34,16 @@ public:
 
 	bool is_deleted() {
 		return this->is_deleted_flag;
+	}
+
+	vector<int>* get_obj_rect() {
+		vector<int> res;
+		res.push_back(this->position[0]), res.push_back(this->size[0]), res.push_back(this->position[1]), res.push_back(this->size[1]);
+		return &res;
+	}
+
+	int get_type() {
+		return this->type;
 	}
 
 	virtual bool update() = 0;
